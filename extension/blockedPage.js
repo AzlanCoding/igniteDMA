bdb2c4f47bad160316cc345767b8e293891cf456
AzlanCoding <101229146+AzlanCoding@users.noreply.github.com>
2024-08-14 09:40:30 +0800

    Improved Formatting Improved formatting of blocked.html by using <code> element instead of double quotes.

--- extension/blockedPage.js
+++ extension/blockedPage.js
@@ -1,6 +1,6 @@
 function addInfo(){
   let url = new URL(window.location.href);
   let params = new URLSearchParams(url.search);
-  document.getElementById('info').innerHTML = "The active profile \""+params.get('profile')+"\" is blocking site access to \""+params.get('site')+"\"";
+  document.getElementById('info').innerHTML = "The active profile <code>"+params.get('profile')+"</code> is blocking site access to <code>"+params.get('site')+"</code>";
 }
 addInfo()
