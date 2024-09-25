4d4dea1e8f2f2402719eb15701256c3722f38c8f
AzlanCoding <101229146+AzlanCoding@users.noreply.github.com>
2024-09-25 17:52:58 +0800

    Fix Popup Bug Fixed error on line 4 of `popupScript.js` whereby could not find windowId as `tabs[0].id` was used instead of `tabs[0].windowId`

--- extension/popupScript.js
+++ extension/popupScript.js
@@ -1,8 +1,7 @@
 function checkAlreadyOpened() {
   chrome.tabs.query({url: chrome.runtime.getURL("options.html")}, function(tabs) {
     if (tabs.length >= 1) {
-      console.log(tabs[0].id);
-      chrome.windows.update(tabs[0].id, {focused: true}, () => {
+      chrome.windows.update(tabs[0].windowId, {focused: true}, () => {
         chrome.tabs.update(tabs[0].id, {active: true}, () => {
           document.getElementById('msg').style["display"] = "block";
         });
